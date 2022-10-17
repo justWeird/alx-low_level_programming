39e424da8a30cef207ff852e38a5d82b2ed90904
Joseph Fadiji <jfblaing997@gmail.com>
2022-10-17 16:17:39 -0700

    Task 4 command hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * main-The entry point function
- * return: 0 if it executes successfully
+/**
+ * main - The entry point function
+ * Return:0 if it executes successfully
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-/* Program terminates */
