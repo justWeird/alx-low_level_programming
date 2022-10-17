410249daa74d09d3b67af3bdfea7eff6f61b23b6
Joseph Fadiji <jfblaing997@gmail.com>
2022-10-17 16:07:15 -0700

    Task 4 command first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
-/* 
- * This is a simple c program designed to output a given line of text. 
- * THis program uses a special function 'puts' instead of 'printf'
- * 
-*/
-
-/* Include the standard i/o header */
 #include <stdio.h>
-
-
-/* Initialise the entry point function - main */
-int main()
+/*
+ * main-The entry point function
+ * return: 0 if it executes successfully
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	/* The '\n' serves a special function to create a new line and '\"' does the same */
-	return 0;
-} 
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 /* Program terminates */
